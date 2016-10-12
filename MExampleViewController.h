92c6a718fa6f518e8a0dfb8f3de1e41f4178a95f
Netset <mobileexpert1234@gmail.com>
2016-10-12 13:18:11 +0530

    Scanner

---
+++ MExampleViewController.h
@@ -0,0 +1,14 @@
+//
+//  MTBAdvancedExampleViewController.h
+//  MTBBarcodeScannerExample
+//
+//  Created by Manpreet Singh on 2/10/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTBAdvancedExampleViewController : UIViewController
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *toggleTorchButton;
+@property (strong, nonatomic) IBOutlet UILabel *labelValue;
+@end
---
+++ MExampleViewController.m
@@ -0,0 +1,257 @@
+//
+//  MTBAdvancedExampleViewController.m
+//  MTBBarcodeScannerExample
+//
+//  Created by Manpreet Singh on 2/10/14.
+//
+//
+
+#import "MTBAdvancedExampleViewController.h"
+#import "MTBBarcodeScanner.h"
+
+@interface MTBAdvancedExampleViewController ()
+
+@property (nonatomic, weak) IBOutlet UIView *previewView;
+@property (nonatomic, weak) IBOutlet UIButton *toggleScanningButton;
+@property (nonatomic, weak) IBOutlet UIView *viewOfInterest;
+
+@property (nonatomic, strong) MTBBarcodeScanner *scanner;
+@property (nonatomic, strong) NSMutableDictionary *overlayViews;
+@property (nonatomic, assign) BOOL didShowAlert;
+@end
+
+@implementation MTBAdvancedExampleViewController
+
+#pragma mark - Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(deviceOrientationDidChange:)
+                                                 name:UIDeviceOrientationDidChangeNotification
+                                               object:nil];
+    
+     [self.navigationItem setRightBarButtonItem:nil];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self.scanner stopScanning];
+}
+
+#pragma mark - Scanner
+
+- (MTBBarcodeScanner *)scanner
+{
+    if (!_scanner)
+    {
+        _scanner = [[MTBBarcodeScanner alloc] initWithPreviewView:_previewView];
+    }
+    return _scanner;
+}
+
+#pragma mark - Overlay Views
+
+- (NSMutableDictionary *)overlayViews
+{
+    if (!_overlayViews) {
+        _overlayViews = [[NSMutableDictionary alloc] init];
+    }
+    return _overlayViews;
+}
+
+#pragma mark - Scanning
+
+- (void)startScanning {
+
+    
+    [self.navigationItem setRightBarButtonItem:self.toggleTorchButton];
+    self.scanner.didStartScanningBlock = ^{
+        NSLog(@"The scanner started scanning!");
+    };
+    
+    self.scanner.didTapToFocusBlock = ^(CGPoint point){
+        NSLog(@"The user tapped the screen to focus. \
+              Here we could present a view at %@", NSStringFromCGPoint(point));
+    };
+    
+    NSError *error;
+    [self.scanner startScanningWithResultBlock:^(NSArray *codes) {
+        [self drawOverlaysOnCodes:codes];
+    } error:&error];
+    
+    if (error) {
+        NSLog(@"An error occurred: %@", error.localizedDescription);
+    }
+    
+    // Optionally set a rectangle of interest to scan codes. Only codes within this rect will be scanned.
+    self.scanner.scanRect = self.viewOfInterest.frame;
+    
+    [self.toggleScanningButton setTitle:@"Stop Scanning" forState:UIControlStateNormal];
+    self.toggleScanningButton.backgroundColor = [UIColor redColor];
+}
+
+- (void)drawOverlaysOnCodes:(NSArray *)codes {
+    // Get all of the captured code strings
+    NSMutableArray *codeStrings = [[NSMutableArray alloc] init];
+    for (AVMetadataMachineReadableCodeObject *code in codes) {
+        if (code.stringValue) {
+            [codeStrings addObject:code.stringValue];
+        }
+    }
+    
+    // Remove any code overlays no longer on the screen
+    for (NSString *code in self.overlayViews.allKeys) {
+        if ([codeStrings indexOfObject:code] == NSNotFound) {
+            // A code that was on the screen is no longer
+            // in the list of captured codes, remove its overlay
+            [self.overlayViews[code] removeFromSuperview];
+            [self.overlayViews removeObjectForKey:code];
+        }
+    }
+    
+    for (AVMetadataMachineReadableCodeObject *code in codes) {
+        UIView *view = nil;
+        NSString *codeString = code.stringValue;
+        
+        if (codeString) {
+            if (self.overlayViews[codeString]) {
+                // The overlay is already on the screen
+                view = self.overlayViews[codeString];
+                
+                // Move it to the new location
+                view.frame = code.bounds;
+                _labelValue.text=codeString;
+                //  }
+                
+            } else {
+                // First time seeing this code
+                BOOL isValidCode = [self isValidCodeString:codeString];
+                
+                // Create an overlay
+                UIView *overlayView = [self overlayForCodeString:codeString
+                                                          bounds:code.bounds
+                                                           valid:isValidCode];
+                self.overlayViews[codeString] = overlayView;
+                
+                // Add the overlay to the preview view
+                [self.previewView addSubview:overlayView];
+            }
+        }
+    }
+}
+
+- (BOOL)isValidCodeString:(NSString *)codeString {
+    BOOL stringIsValid = ([codeString rangeOfString:@"Valid"].location != NSNotFound);
+    return stringIsValid;
+}
+
+- (UIView *)overlayForCodeString:(NSString *)codeString bounds:(CGRect)bounds valid:(BOOL)valid {
+    UIColor *viewColor = valid ? [UIColor greenColor] : [UIColor redColor];
+    UIView *view = [[UIView alloc] initWithFrame:bounds];
+    UILabel *label = [[UILabel alloc] initWithFrame:view.bounds];
+    
+    // Configure the view
+    view.layer.borderWidth =10.0;
+    view.backgroundColor = [viewColor colorWithAlphaComponent:0.75];
+    view.layer.borderColor = viewColor.CGColor;
+    
+    // Configure the label
+    label.font = [UIFont boldSystemFontOfSize:12];
+    label.text = codeString;
+    label.textColor = [UIColor blackColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.numberOfLines = 0;
+    
+    // Add constraints to label to improve text size?
+    
+    // Add the label to the view
+    [view addSubview:label];
+    
+    return view;
+}
+
+- (void)stopScanning
+{
+    [self.scanner stopScanning];
+    _labelValue.text = nil;
+     [self.navigationItem setRightBarButtonItem:nil];
+    [self.toggleScanningButton setTitle:@"Start Scanning" forState:UIControlStateNormal];
+    self.toggleScanningButton.backgroundColor = self.view.tintColor;
+    
+    for (NSString *code in self.overlayViews.allKeys) {
+        [self.overlayViews[code] removeFromSuperview];
+    }
+}
+
+#pragma mark - Actions
+
+- (IBAction)toggleScanningTapped:(id)sender {
+    if ([self.scanner isScanning]) {
+        [self stopScanning];
+         self.toggleTorchButton.title = @"Enable Torch";
+    } else {
+        [MTBBarcodeScanner requestCameraPermissionWithSuccess:^(BOOL success) {
+            if (success) {
+                [self startScanning];
+            } else {
+                [self displayPermissionMissingAlert];
+            }
+        }];
+    }
+}
+
+- (IBAction)switchCameraTapped:(id)sender {
+    [self.scanner flipCamera];
+}
+
+- (void)backTapped {
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Notifications
+
+- (void)deviceOrientationDidChange:(NSNotification *)notification {
+    self.scanner.scanRect = self.viewOfInterest.frame;
+}
+
+#pragma mark - Helper Methods
+
+- (void)displayPermissionMissingAlert {
+    NSString *message = nil;
+    if ([MTBBarcodeScanner scanningIsProhibited]) {
+        message = @"This app does not have permission to use the camera.";
+    } else if (![MTBBarcodeScanner cameraIsPresent]) {
+        message = @"This device does not have a camera.";
+    } else {
+        message = @"An unknown error occurred.";
+    }
+    
+    [[[UIAlertView alloc] initWithTitle:@"Scanning Unavailable"
+                                message:message
+                               delegate:nil
+                      cancelButtonTitle:@"Ok"
+                      otherButtonTitles:nil] show];
+}
+
+
+- (IBAction)toggleTorchTapped:(id)sender {
+    if (self.scanner.torchMode == MTBTorchModeOff || self.scanner.torchMode == MTBTorchModeAuto) {
+        self.scanner.torchMode = MTBTorchModeOn;
+        self.toggleTorchButton.title = @"Disable Torch";
+    } else {
+        self.scanner.torchMode = MTBTorchModeOff;
+        self.toggleTorchButton.title = @"Enable Torch";
+    }
+}
+@end
